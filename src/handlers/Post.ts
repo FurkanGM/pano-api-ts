3467889a0d0d0b941ccc7454faeb317ebf6c6571
Furkan Gezek <furkangezek2014@gmail.com>
2023-04-08 01:18:37 +0300

    wip

--- src/handlers/Post.ts
+++ src/handlers/Post.ts
@@ -2,18 +2,6 @@ import {TwirpContext} from "twirp-ts";
 import {GetPostsRequest, GetPostsResponse} from "../../protos/service";
 
 const GetPosts = async (ctx: TwirpContext, request: GetPostsRequest): Promise<GetPostsResponse> => {
-    console.log(GetPostsResponse.fromJSON({
-        posts: [
-            {
-                id: 'test',
-                title: 'test',
-                url: 'test',
-                content: 'test',
-                slug: 'test',
-                userId: 'test',
-            }
-        ]
-    }))
     return GetPostsResponse.fromJSON({
         posts: [
             {
