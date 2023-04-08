172b19b5d33d6086bc41adf751d743e87e02072e
Furkan Gezek <furkangezek2014@gmail.com>
2023-04-08 13:32:48 +0300

    create missing post handlers

--- src/handlers/Post.ts
+++ src/handlers/Post.ts
@@ -1,52 +1,116 @@
 import {TwirpContext} from "twirp-ts";
-import {CreatePostRequest, CreatePostResponse, GetPostsRequest, GetPostsResponse} from "../protos/service";
+import {
+    CreatePostRequest,
+    CreatePostResponse, DeletePostRequest, DeletePostResponse,
+    GetBatchPostsRequest,
+    GetBatchPostsResponse,
+    GetPostsRequest,
+    GetPostsResponse,
+    UpdatePostRequest,
+    UpdatePostResponse
+} from "../protos/service";
 import {PanoAPITwirp} from "../protos/service.twirp";
 import {prisma} from "../index";
 import {slugify} from "../support/Helper";
 
-const GetPosts = async (ctx: TwirpContext, request: GetPostsRequest): Promise<GetPostsResponse> => {
-    return GetPostsResponse.fromJson({
-        posts: [
-            {
-                id: 'test',
-                title: 'test',
-                url: 'test',
-                content: 'test',
-                slug: 'test',
-                userId: 'test',
-            }
-        ]
+const CreatePost = async (ctx: TwirpContext, request: CreatePostRequest): Promise<CreatePostResponse> => {
+    const post = await prisma.post.create({
+        data: {
+            title: request.title,
+            slug: slugify(request.title),
+            url: request.url,
+            content: request.content,
+            userID: request.userId
+        }
+    })
+
+    return CreatePostResponse.fromJson({
+        post: {
+            id: post.id,
+            title: post.title,
+            url: post.url,
+            content: post.content,
+            slug: post.slug,
+            user_id: post.userID
+        }
     })
 }
 
-const CreatePost = async (ctx: TwirpContext, request: CreatePostRequest): Promise<CreatePostResponse> => {
-    try {
-        const post = await prisma.post.create({
-            data: {
-                title: request.title,
-                slug: slugify(request.title),
-                url: request.url,
-                content: request.content,
-                userID: request.userId
-            }
-        })
+const GetPosts = async (ctx: TwirpContext, request: GetPostsRequest): Promise<GetPostsResponse> => {
+    const posts = await prisma.post.findMany({
+        ...(request.limit ? {take: request.limit} : {}),
+        ...(request.offset ? {skip: request.offset} : {})
+    })
 
-        return CreatePostResponse.fromJson({
-            post: {
-                id: post.id,
-                title: post.title,
-                url: post.url,
-                content: post.content,
-                slug: post.slug,
-                user_id: post.userID
+    return GetPostsResponse.fromJson({
+        posts: posts.map(post => ({
+            id: post.id,
+            title: post.title,
+            url: post.url,
+            content: post.content,
+            slug: post.slug,
+            user_id: post.userID
+        }))
+    })
+}
+
+const GetBatchPosts = async (ctx: TwirpContext, request: GetBatchPostsRequest): Promise<GetBatchPostsResponse> => {
+    const posts = await prisma.post.findMany({
+        where: {
+            id: {
+                in: request.ids
             }
-        })
-    } catch (e) {
-        throw e
-    }
+        }
+    })
+
+    return GetPostsResponse.fromJson({
+        posts: posts.map(post => ({
+            id: post.id,
+            title: post.title,
+            url: post.url,
+            content: post.content,
+            slug: post.slug,
+            user_id: post.userID
+        }))
+    })
+}
+
+const UpdatePost = async (ctx: TwirpContext, request: UpdatePostRequest): Promise<UpdatePostResponse> => {
+    const updatePost = await prisma.post.update({
+        where: {
+            id: request.id
+        },
+        data: {
+            ...(request.title ? {
+                title: request.title.value,
+                slug: slugify(request.title.value)
+            } : {}),
+            ...(request.url ? {
+                url: request.url.value,
+            } : {}),
+            ...(request.content ? {
+                content: request.content.value,
+            } : {}),
+        }
+    })
+
+    return UpdatePostResponse.fromJson({})
+}
+
+const DeletePost = async (ctx: TwirpContext, request: DeletePostRequest): Promise<DeletePostResponse> => {
+    const deletePost = await prisma.post.delete({
+        where: {
+            id: request.id
+        }
+    })
+
+    return DeletePostResponse.fromJson({})
 }
 
 export default {
+    CreatePost,
     GetPosts,
-    CreatePost
+    GetBatchPosts,
+    UpdatePost,
+    DeletePost
 } as PanoAPITwirp
