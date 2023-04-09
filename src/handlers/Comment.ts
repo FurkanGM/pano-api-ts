694c3a0e842c05f8e14e474ef7bd60e8c76d0822
Furkan Gezek <furkangezek2014@gmail.com>
2023-04-09 04:54:20 +0300

    styling

--- src/handlers/Comment.ts
+++ src/handlers/Comment.ts
@@ -1,104 +1,119 @@
-import {PanoAPITwirp} from "../protos/service.twirp";
-import {TwirpContext} from "twirp-ts";
+import { PanoAPITwirp } from "../protos/service.twirp"
+import { TwirpContext } from "twirp-ts"
 import {
-    CreateCommentRequest,
-    CreateCommentResponse,
-    DeleteCommentRequest,
-    DeleteCommentResponse,
-    GetBatchCommentsRequest,
-    GetBatchCommentsResponse,
-    GetCommentsRequest,
-    GetCommentsResponse,
-    UpdateCommentRequest,
-    UpdateCommentResponse
-} from "../protos/service";
-import {prisma} from "../index";
+	CreateCommentRequest,
+	CreateCommentResponse,
+	DeleteCommentRequest,
+	DeleteCommentResponse,
+	GetBatchCommentsRequest,
+	GetBatchCommentsResponse,
+	GetCommentsRequest,
+	GetCommentsResponse,
+	UpdateCommentRequest,
+	UpdateCommentResponse,
+} from "../protos/service"
+import { prisma } from "../index"
 
-const CreateComment = async (ctx: TwirpContext, request: CreateCommentRequest): Promise<CreateCommentResponse> => {
-    const comment = await prisma.comment.create({
-        data: {
-            content: request.content,
-            userID: request.userId,
-            postID: request.postId,
-            parentID: request.parentId?.value
-        }
-    })
+const CreateComment = async (
+	ctx: TwirpContext,
+	request: CreateCommentRequest
+): Promise<CreateCommentResponse> => {
+	const comment = await prisma.comment.create({
+		data: {
+			content: request.content,
+			userID: request.userId,
+			postID: request.postId,
+			parentID: request.parentId?.value,
+		},
+	})
 
-    return CreateCommentResponse.fromJson({
-        comment: {
-            id: comment.id,
-            content: comment.content,
-            userId: comment.userID,
-            postId: comment.postID,
-            parentId: comment.parentID
-        }
-    })
+	return CreateCommentResponse.fromJson({
+		comment: {
+			id: comment.id,
+			content: comment.content,
+			userId: comment.userID,
+			postId: comment.postID,
+			parentId: comment.parentID,
+		},
+	})
 }
 
-const GetComments = async (ctx: TwirpContext, request: GetCommentsRequest): Promise<GetCommentsResponse> => {
-    const comments = await prisma.comment.findMany({
-        ...(request.limit ? {take: request.limit} : {}),
-        ...(request.offset ? {skip: request.offset} : {})
-    })
+const GetComments = async (
+	ctx: TwirpContext,
+	request: GetCommentsRequest
+): Promise<GetCommentsResponse> => {
+	const comments = await prisma.comment.findMany({
+		...(request.limit ? { take: request.limit } : {}),
+		...(request.offset ? { skip: request.offset } : {}),
+	})
 
-    return GetCommentsResponse.fromJson({
-        comments: comments.map(comment => ({
-            id: comment.id,
-            content: comment.content,
-            userId: comment.userID,
-            postId: comment.postID,
-            parentId: comment.parentID
-        }))
-    })
+	return GetCommentsResponse.fromJson({
+		comments: comments.map((comment) => ({
+			id: comment.id,
+			content: comment.content,
+			userId: comment.userID,
+			postId: comment.postID,
+			parentId: comment.parentID,
+		})),
+	})
 }
 
-const GetBatchComments = async (ctx: TwirpContext, request: GetBatchCommentsRequest): Promise<GetBatchCommentsResponse> => {
-    const comments = await prisma.comment.findMany({
-        where: {
-            id: {
-                in: request.ids
-            }
-        }
-    })
+const GetBatchComments = async (
+	ctx: TwirpContext,
+	request: GetBatchCommentsRequest
+): Promise<GetBatchCommentsResponse> => {
+	const comments = await prisma.comment.findMany({
+		where: {
+			id: {
+				in: request.ids,
+			},
+		},
+	})
 
-    return GetBatchCommentsResponse.fromJson({
-        comments: comments.map(comment => ({
-            id: comment.id,
-            content: comment.content,
-            userId: comment.userID,
-            postId: comment.postID,
-            parentId: comment.parentID
-        }))
-    })
+	return GetBatchCommentsResponse.fromJson({
+		comments: comments.map((comment) => ({
+			id: comment.id,
+			content: comment.content,
+			userId: comment.userID,
+			postId: comment.postID,
+			parentId: comment.parentID,
+		})),
+	})
 }
 
-const UpdateComment = async (ctx: TwirpContext, request: UpdateCommentRequest): Promise<UpdateCommentResponse> => {
-    const updateComment = await prisma.comment.update({
-        where: {
-            id: request.id
-        },
-        data: {
-            content: request.content
-        }
-    })
+const UpdateComment = async (
+	ctx: TwirpContext,
+	request: UpdateCommentRequest
+): Promise<UpdateCommentResponse> => {
+	const updateComment = await prisma.comment.update({
+		where: {
+			id: request.id,
+		},
+		data: {
+			content: request.content,
+		},
+	})
 
-    return UpdateCommentResponse.fromJson({})
+	return UpdateCommentResponse.fromJson({})
 }
 
-const DeleteComment = async (ctx: TwirpContext, request: DeleteCommentRequest): Promise<DeleteCommentResponse> => {
-    const deleteComment = await prisma.comment.delete({
-        where: {
-            id: request.id
-        }
-    })
+const DeleteComment = async (
+	ctx: TwirpContext,
+	request: DeleteCommentRequest
+): Promise<DeleteCommentResponse> => {
+	const deleteComment = await prisma.comment.delete({
+		where: {
+			id: request.id,
+		},
+	})
 
-    return DeleteCommentResponse.fromJson({})
+	return DeleteCommentResponse.fromJson({})
 }
 
 export default {
-    CreateComment,
-    GetComments,
-    GetBatchComments,
-    UpdateComment,
-    DeleteComment,
+	CreateComment,
+	GetComments,
+	GetBatchComments,
+	UpdateComment,
+	DeleteComment,
 } as PanoAPITwirp
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import Post from "./handlers/Post";
-import {createPanoAPIServer, PanoAPITwirp} from "./protos/service.twirp";
-import {PrismaClient} from '@prisma/client'
-import Upvote from "./handlers/Upvote";
+import express from "express"
+import dotenv from "dotenv"
+import Post from "./handlers/Post"
+import { createPanoAPIServer, PanoAPITwirp } from "./protos/service.twirp"
+import { PrismaClient } from "@prisma/client"
+import Upvote from "./handlers/Upvote"
+import Comment from "./handlers/Comment"
 
-dotenv.config();
+dotenv.config()
 
 const server = createPanoAPIServer({
-    ...Post,
-    ...Comment,
-    ...Upvote
-} as PanoAPITwirp);
+	...Post,
+	...Comment,
+	...Upvote,
+} as PanoAPITwirp)
 
-const app = express();
-const prisma = new PrismaClient();
+const app = express()
+const prisma = new PrismaClient()
 
-app.post(server.matchingPath(), server.httpHandler());
+app.post(server.matchingPath(), server.httpHandler())
 
 app.listen(3000, () => {
-    console.log(`Server is running at :3000`);
-});
+	console.log(`Server is running at :3000`)
+})
 
-export {
-    prisma
-}
+export { prisma }
